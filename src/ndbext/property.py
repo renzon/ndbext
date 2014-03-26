8687676e457ec82a4d375715783617aee52e5aa6
renzon <renzo@gmail.com>
2014-03-26 16:19:49 -0300

    close #1

--- src/ndbext/property.py
+++ src/ndbext/property.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
 
+
+class BoundaryError(Exception):
+    pass
+
+
+class IntegerBoundary(ndb.IntegerProperty):
+    def __init__(self, lower=None, upper=None, **kwargs):
+        self.upper = upper
+        self.lower = lower
+        super(IntegerBoundary, self).__init__(**kwargs)
+
+    def _validate(self, value):
+        if self.lower is not None and value < self.lower:
+            raise BoundaryError('%s is less then %s' % (value, self.lower))
+        if self.upper is not None and value > self.upper:
+            raise BoundaryError('%s is greater then %s' % (value, self.upper))
---
+++ test/property_tests.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from string import lower
+from google.appengine.ext import ndb
+from ndbext.property import IntegerBoundary, BoundaryError
+from util import GAETestCase
+
+
+class ModelMock(ndb.Model):
+    lower = IntegerBoundary(lower=0)
+    upper = IntegerBoundary(upper=3)
+    lower_and_upper = IntegerBoundary(lower=-1, upper=1)
+
+
+class IntegerBoundaryTests(GAETestCase):
+    def test_lower(self):
+        self.assertRaises(BoundaryError, ModelMock, lower=-1)
+
+        # no exceptions with bigger values
+        ModelMock(lower=0)
+        ModelMock(lower=99999999999999999999)
+
+    def test_upper(self):
+        self.assertRaises(BoundaryError, ModelMock, upper=4)
+
+        # no exceptions with bigger values
+        ModelMock(upper=3)
+        ModelMock(upper=-99999999999999999999)
+
+
+
+
